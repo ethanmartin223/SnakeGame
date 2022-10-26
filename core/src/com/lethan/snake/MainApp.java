b98725bfdb2efa0ab2a6ff3220ff04227942d234
ethanmartin223 <80828278+ethanmartin223@users.noreply.github.com>
2022-10-26 13:57:45 -0400

    Updated Code in Main

--- core/src/com/lethan/snake/MainApp.java
+++ core/src/com/lethan/snake/MainApp.java
@@ -12,20 +12,17 @@ public class MainApp extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
