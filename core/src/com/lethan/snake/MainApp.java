0d577effbe0edf01ef415b0089a8c62875414b37
ethanmartin223 <80828278+ethanmartin223@users.noreply.github.com>
2022-10-27 08:15:01 -0400

    Updated Code

--- core/src/com/lethan/snake/MainApp.java
+++ core/src/com/lethan/snake/MainApp.java
@@ -4,20 +4,29 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MainApp extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	ShapeRenderer shapeRender;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		shapeRender = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
+
 		batch.begin();
+
+		shapeRender.begin(ShapeRenderer.ShapeType.Filled);
+
+		shapeRender.end();
+
 		batch.end();
 	}
 	
